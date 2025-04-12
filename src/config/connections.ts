ada97598262b5b298fd2090ac2799ea16e8d7b12
heimsharon <heimsharon@hotmail.com>
2025-04-12 07:36:32 -0400

    user updates/connection

--- src/config/connections.ts
+++ src/config/connections.ts
@@ -1,5 +1,5 @@
 import mongoose, { Mongoose } from "mongoose";
 
-mongoose.connect('monogodb://localhost:27017/monorepo');
+mongoose.connect('monogodb://localhost:27017/your_database_name', );
 
 export default mongoose.connection;
--- src/models/User.ts
+++ src/models/User.ts
@@ -8,37 +8,39 @@ interface IUser extends Document {
 }
 
 const UserSchema = new Schema<IUser>({
-
   username: {
     type: String,
     unique: true,
     required: true,
-    trim: true
+    trim: true,
   },
 
   email: {
     type: String,
     required: true,
     unique: true,
-    match: [ /.+@.+\..+/, 'Must match an email address!' ]
+    match: [/^\S+@\S+\.\S+$/, 'Must match a valid email address!'], 
   },
 
-  thoughts: [ {
-    type: Schema.Types.ObjectId,
-    ref: 'Thought'
-  } ],
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
 
-  friends: [ {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  } ],
-
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
 });
 
-UserSchema.virtual('friendCount').get
-  (function () {
-    return this.friends.length;
-  });
+// Virtual property for friend count
+UserSchema.virtual('friendCount').get(function (this: IUser) {
+  return this.friends.length;
+});
 
 const User = mongoose.model<IUser>('User', UserSchema);
 
